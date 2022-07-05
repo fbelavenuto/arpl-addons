77876260fb23a8c1fca17c30b09d7cbd454acf56
Fabio Belavenuto <belavenuto@gmail.com>
2022-07-05 16:57:21 -0300

    Fixing acpid addon

--- acpid/install.sh
+++ acpid/install.sh
@@ -1,12 +1,11 @@
-if [ "${1}" = "sys" ]; then
-  echo "Installing module and daemon for ACPI button"
-  if [ ! -f /tmpRoot/lib/modules/button.ko ]; then
-    cp /modules/button.ko /tmpRoot/lib/modules/
-  fi
+if [ "${1}" = "rd" ]; then
+  ${INSMOD} /modules/button.ko ${PARAMS}
+else
+  echo "Installing daemon for ACPI button"
   tar -zxvf /addons/acpid.tgz -C /tmpRoot/
   chmod 755 /tmpRoot/usr/sbin/acpid
   chmod 644 /tmpRoot/etc/acpi/events/power
-  chmod 744 /tmpRoot/etc/acpi/power.sh
-  chmod 744 /tmpRoot/usr/lib/systemd/system/acpid.service
+  chmod 755 /tmpRoot/etc/acpi/power.sh
+  chmod 644 /tmpRoot/usr/lib/systemd/system/acpid.service
   ln -sf /usr/lib/systemd/system/acpid.service /tmpRoot/etc/systemd/system/multi-user.target.wants/acpid.service
 fi
