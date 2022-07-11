8faea4f8fe0aa5da3e63d3784f3d5df36e7d711a
Fabio Belavenuto <belavenuto@gmail.com>
2022-07-11 17:02:55 -0300

    Fixing mpt3sas

--- mpt3sas/install.sh
+++ mpt3sas/install.sh
@@ -2,5 +2,7 @@
 
 if [ "${1}" = "early" ]; then
   echo "Installing module for LSI MPT Fusion SAS 3.0 Device adapter"
+  ${INSMOD} "/modules/raid_class.ko"
+  ${INSMOD} "/modules/scsi_transport_sas.ko"
   ${INSMOD} "/modules/mpt3sas.ko" ${PARAMS}
 fi
