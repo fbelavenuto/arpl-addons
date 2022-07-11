54786ed652ec799171dccd56a6281fd3748e54db
Fabio Belavenuto <belavenuto@gmail.com>
2022-07-11 12:08:04 -0300

    Fixing alx

--- alx/install.sh
+++ alx/install.sh
@@ -2,5 +2,6 @@
 
 if [ "${1}" = "early" ]; then
   echo "Installing module for Qualcomm Atheros AR816x/AR817x ethernet adapters"
+  ${INSMOD} "/modules/mdio.ko"
   ${INSMOD} "/modules/alx.ko" ${PARAMS}
 fi
