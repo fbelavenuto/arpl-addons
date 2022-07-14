a89632ae94c307959ddccce2b512bf7aa37ea228
Fabio Belavenuto <belavenuto@gmail.com>
2022-07-13 21:59:12 -0300

    Added eudev

---
+++ eudev/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env ash
+
+if [ "${1}" = "jrExit" ]; then
+  echo "Starting eudev daemon"
+  [ -e /proc/sys/kernel/hotplug ] && printf '\000\000\000\000' > /proc/sys/kernel/hotplug
+  /sbin/udevd -d || { echo "FAIL"; exit 1; }
+  echo "Triggering add events to udev"
+  udevadm trigger --type=subsystems --action=add
+  udevadm trigger --type=devices --action=add
+  udevadm settle --timeout=10 || echo "udevadm settle failed"
+fi
