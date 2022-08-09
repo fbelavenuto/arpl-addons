e67db8ca2967dbdaa4466739d0a6924b324ac0e6
Fabio Belavenuto <belavenuto@gmail.com>
2022-08-09 11:52:20 -0300

    dtbpatch wip

--- dtbpatch/install.sh
+++ dtbpatch/install.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env ash
 
+# Detect correct file
+HW_REVISION=`cat /proc/sys/kernel/syno_hw_revision`
+[ -n "${HW_REVISION}" ] && DTBFILE="model_${HW_REVISION}.dtb" || DTBFILE="model.dtb"
+[ -e /etc.defaults/${DTBFILE} ] || DTBFILE="model.dtb"
+
 if [ "${1}" = "patches" ]; then
   echo "dtbpatch - patches"
   # fix executable flag
   chmod +x /usr/sbin/dtbpatch
 
+  echo "Patching /etc.defaults/${DTBFILE}"
+
   # Dynamic generation
-  dtbpatch /etc.defaults/model.dtb output.dtb
-  if [ $? -ne 0 ]; then
-    echo "Error patching dtb"
+  if dtbpatch /etc.defaults/${DTBFILE} /var/run/model.dtb; then
+    cp -f /var/run/model.dtb /etc.defaults/${DTBFILE}
   else
-    cp -vf output.dtb /etc.defaults/model.dtb
-    cp -vf output.dtb /var/run/model.dtb
+    echo "Error patching dtb"
   fi
 elif [ "${1}" = "late" ]; then
   echo "dtbpatch - late"
+  echo "Copying /etc.defaults/${DTBFILE}"
   # copy file
-  cp -vf /etc.defaults/model.dtb /tmpRoot/etc.defaults/model.dtb
+  cp -vf /etc.defaults/${DTBFILE} /tmpRoot/etc.defaults/model.dtb
 fi
