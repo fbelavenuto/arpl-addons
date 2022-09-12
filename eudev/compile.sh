756e51f6520cdd0751fe5d5053c627853ed1624e
Fabio Belavenuto <belavenuto@gmail.com>
2022-09-12 08:13:41 -0300

    Trying fix eudev compilation

--- eudev/compile.sh
+++ eudev/compile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 #[ -f all/usr/bin/kmod -a -f all/usr/bin/udevadm ] && exit 0
-docker run --rm -t -v $PWD/src:/input -v $PWD/all:/output fbelavenuto/syno-compiler bash /input/docker-compile.sh
+docker run -u 1000 --rm -t -v $PWD/src:/input -v $PWD/all:/output fbelavenuto/syno-compiler bash /input/docker-compile.sh
 sudo chown `id -u`:`id -g` -R all
