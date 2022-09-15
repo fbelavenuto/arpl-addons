ea9ccb1b6ab8221f5a2a63545d559dfae7563175
Fabio Belavenuto <belavenuto@gmail.com>
2022-09-15 16:30:50 -0300

    Fixing eudev bug with RAID mount scripts

--- eudev/compile.sh
+++ eudev/compile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#[ -f all/usr/bin/kmod -a -f all/usr/bin/udevadm ] && exit 0
+[ -f all/usr/bin/kmod -a -f all/usr/bin/udevadm ] && exit 0
 mkdir all
 sudo chown 1000 -R all
 docker run -u 1000 --rm -t -v $PWD/src:/input -v $PWD/all:/output fbelavenuto/syno-compiler bash /input/docker-compile.sh
