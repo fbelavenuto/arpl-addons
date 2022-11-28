08a72ce8d19647ace80aed300b23e9432a09415d
Fabio Belavenuto <belavenuto@gmail.com>
2022-11-28 10:54:36 -0300

    Changing addon systemd.timer to crontab

--- powersched/install.sh
+++ powersched/install.sh
@@ -4,32 +4,8 @@ if [ "${1}" = "late" ]; then
   echo "Installing powersched tools"
   cp -vf /usr/sbin/powersched /tmpRoot/usr/sbin/powersched
   chmod 755 /tmpRoot/usr/sbin/powersched
-  cat > /tmpRoot/etc/systemd/system/powersched.timer <<'EOF'
-[Unit]
-Description=Configure RTC to DSM power schedule
-
-[Timer]
-OnCalendar=*-*-* *:*:30
-Persistent=true
-
-[Install]
-WantedBy=timers.target
-EOF
-  mkdir -p /tmpRoot/etc/systemd/system/timers.target.wants
-  ln -sf /etc/systemd/system/powersched.timer /tmpRoot/etc/systemd/system/timers.target.wants/powersched.timer
-  cat > /tmpRoot/etc/systemd/system/powersched.service <<'EOF'
-[Unit]
-Description=Configure RTC to DSM power schedule
-
-[Service]
-Type=oneshot
-ExecStart=/usr/sbin/powersched
-
-[Install]
-WantedBy=multi-user.target
-EOF
-  mkdir -p /tmpRoot/etc/systemd/system/multi-user.target.wants
-  ln -sf /etc/systemd/system/powersched.service /tmpRoot/etc/systemd/system/multi-user.target.wants/powersched.service
+  # Add line to crontab, execute each minute
+  echo "*       *       *       *       *       root    /usr/sbin/powersched" >> /tmpRoot/etc/crontab
   # Reduce the systemd log level
   if grep -q "^LogLevel=" /tmpRoot/etc/systemd/system.conf; then
     sed 's\^LogLevel=.*\LogLevel=notice' -i /tmpRoot/etc/systemd/system.conf
