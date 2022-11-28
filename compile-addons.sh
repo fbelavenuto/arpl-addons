eae441c3b8dee7910f340294c5c9ca6bfdb3152e
Fabio Belavenuto <belavenuto@gmail.com>
2022-11-28 10:34:11 -0300

    testing

--- compile-addons.sh
+++ compile-addons.sh
@@ -142,13 +142,16 @@ function compile-addon() {
     COPY_PATH="`readConfigKey 'available-for."'${P}'".copy' "${MANIFEST}"`"
     # If folder exists, copy
     if [ -n "${COPY_PATH}" ]; then
+      echo "${COPY_PATH}"
+      ls -la "${1}"
+      ls -la "${1}/${COPY_PATH}"
       if [ -d "${1}/${COPY_PATH}" ]; then
         echo -e "\033[1;35m  Copying folder '${COPY_PATH}'\033[0m"
         mkdir -p "${OUT_PATH}/${P}/root"
         cp -R "${1}/${COPY_PATH}/"* "${OUT_PATH}/${P}/root"
         HAS_FILES=1
       else
-        echo -e "\033[1;33m  WARNING: folder '${COPY_PATH}' not found\033[0m"
+        echo -e "\033[1;33m  WARNING: folder '${1}/${COPY_PATH}' not found\033[0m"
       fi
     fi
     HAS_MODULES="`readConfigKey 'available-for."'${P}'".modules' "${MANIFEST}"`"
