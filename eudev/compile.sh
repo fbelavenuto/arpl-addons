086bdb4a24df45e3142feb2596a1947b2af2374f
Fabio Belavenuto <belavenuto@gmail.com>
2022-11-28 09:38:02 -0300

    Adding r1000 platform

--- eudev/compile.sh
+++ eudev/compile.sh
@@ -3,5 +3,5 @@
 [ -f all/usr/bin/kmod -a -f all/usr/bin/udevadm ] && exit 0
 mkdir all
 sudo chown 1000 -R all
-docker run -u 1000 --rm -t -v $PWD/src:/input -v $PWD/all:/output fbelavenuto/syno-compiler bash /input/docker-compile.sh
+docker run --rm -t -v $PWD/src:/input -v $PWD/all:/output fbelavenuto/syno-toolkit:apollolake-7.1 bash /input/docker-compile.sh
 sudo chown 1000 -R all
