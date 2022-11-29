eeee46e8b58f94b2200247048262a4d46616003f
Fabio Belavenuto <belavenuto@gmail.com>
2022-11-29 12:29:38 -0300

    Make powersched configurable and add reducelog addon

--- powersched/install.sh
+++ powersched/install.sh
@@ -6,16 +6,4 @@ if [ "${1}" = "late" ]; then
   chmod 755 /tmpRoot/usr/sbin/powersched
   # Add line to crontab, execute each minute
   echo "*       *       *       *       *       root    /usr/sbin/powersched" >> /tmpRoot/etc/crontab
-  # Reduce the systemd log level
-  if grep -q "^LogLevel=" /tmpRoot/etc/systemd/system.conf; then
-    sed 's\^LogLevel=.*\LogLevel=notice' -i /tmpRoot/etc/systemd/system.conf
-  else
-    echo "LogLevel=notice" >> /tmpRoot/etc/systemd/system.conf
-  fi
-  for path in /tmpRoot/etc.defaults /tmpRoot/etc
-  do
-    sed 's|destination(d_scemd);|flags(final);|'       -i ${path}/syslog-ng/patterndb.d/scemd.conf
-    sed 's|destination(d_synosystemd);|flags(final);|' -i ${path}/syslog-ng/patterndb.d/synosystemd.conf
-    sed 's|destination(d_systemd);|flags(final);|'     -i ${path}/syslog-ng/patterndb.d/synosystemd.conf
-  done
 fi
