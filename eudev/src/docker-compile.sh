ce2fffa0e58605b337e70cc357b4120ffe371c1b
Fabio Belavenuto <belavenuto@gmail.com>
2022-11-30 13:31:35 -0300

    Fixing missing libblkid.so.1 to eudev addon

--- eudev/src/docker-compile.sh
+++ eudev/src/docker-compile.sh
@@ -11,15 +11,16 @@ patch -p1 < /input/kmod.patch
 ./autogen.sh
 ./configure CFLAGS='-O2' --prefix=/usr --sysconfdir=/etc --libdir=/usr/lib --enable-tools --disable-manpages --disable-python --without-zstd --without-xz --without-zlib --without-openssl
 make all
-sudo make install
+make install
 make DESTDIR=/output install
 git clone -c http.sslVerify=false --single-branch https://github.com/eudev-project/eudev.git /tmp/eudev
 cd /tmp/eudev
 git checkout v3.2.11
 ./autogen.sh
 ./configure --prefix=/usr --sysconfdir=/etc --disable-manpages --disable-selinux --disable-mtd_probe --enable-kmod
-make all
-make DESTDIR=/output install
+make -i all
+make -i DESTDIR=/output install
 rm -Rf /output/usr/share /output/usr/include /output/usr/lib/pkgconfig /output/usr/lib/libudev.*
 rm /output/usr/lib/udev/rules.d/80-net-name-slot.rules
 ln -sf /usr/bin/kmod /output/usr/sbin/depmod
+chown 1000.1000 -R /output
