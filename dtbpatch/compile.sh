ec74066456eadcb2983bf6bec1d2942748cf4870
Fabio Belavenuto <belavenuto@gmail.com>
2022-11-30 14:55:16 -0300

    little fixes

--- dtbpatch/compile.sh
+++ dtbpatch/compile.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+[ -f all/usr/sbin/dtbpatch ] && exit 0
+
 mkdir -p all/usr/sbin
 make -C src clean all
 cp src/dtbpatch all/usr/sbin
--- powersched/compile.sh
+++ powersched/compile.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+[ -f all/usr/sbin/powersched ] && exit 0
+
 mkdir -p all/usr/sbin
 make -C src clean all
 cp src/powersched all/usr/sbin
