da5f0375d24011122e57388f5fc8f7474e2907cb
Fabio Belavenuto <belavenuto@gmail.com>
2023-01-11 20:53:46 -0300

    misc addon now copies cpufreq modules to DSM Removing old addons, they returns withou authorization :)

--- remove-pma/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ash
-
-if [ "${1}" = "late" ]; then
-  echo "Installing remove-pma script"
-  # Add line to crontab, execute each hour
-  echo "0       *       *       *       *       root    /usr/bin/find /volume1/@appstore/SurveillanceStation/local_display/.config/chromium-local-display/BrowserMetrics -name \"*.pma\" -delete" >> /tmpRoot/etc/crontab
-fi
