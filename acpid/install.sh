c2c91b58c935eacb0016c5c0304c428a1ed75a44
Fabio Belavenuto <belavenuto@gmail.com>
2023-01-23 09:10:04 -0300

    Removing acpid from junior step

--- acpid/install.sh
+++ acpid/install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env ash
 
-if [ "${1}" = "early" ]; then
-  /usr/sbin/acpid
-elif [ "${1}" = "late" ]; then
-  /usr/bin/killall acpid
+#if [ "${1}" = "early" ]; then
+  #/usr/sbin/acpid
+#el
+if [ "${1}" = "late" ]; then
+  #/usr/bin/killall acpid
   echo "Installing daemon for ACPI button"
   cp -v /usr/sbin/acpid /tmpRoot/usr/sbin/acpid
   mkdir -p /tmpRoot/etc/acpi/events/
