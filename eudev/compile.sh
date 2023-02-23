703b58bccf2f50c94ada71515db8094b6c405cac
Fabio Belavenuto <belavenuto@gmail.com>
2023-02-23 15:53:32 -0300

    Adding toolkit version to docker image tag

--- eudev/compile.sh
+++ eudev/compile.sh
@@ -3,5 +3,7 @@
 [ -f all/usr/bin/kmod -a -f all/usr/bin/udevadm ] && exit 0
 mkdir all
 sudo chown 1000 -R all
-docker run --rm -t -v $PWD/src:/input -v $PWD/all:/output fbelavenuto/syno-toolkit:apollolake-7.1 shell /input/docker-compile.sh
+TOOLKIT_VER="7.1"
+#docker run --rm -t -v $PWD/src:/input -v $PWD/all:/output fbelavenuto/syno-toolkit:apollolake-7.1 shell /input/docker-compile.sh
+docker run --rm -t -v $PWD/src:/input -v $PWD/all:/output fbelavenuto/syno-compiler:${TOOLKIT_VER} shell apollolake /input/docker-compile.sh
 sudo chown 1000 -R all
