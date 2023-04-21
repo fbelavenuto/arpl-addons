24212fdc1c24972b98f3523d1523b4551ea78d53
AuxXxilium <info@visionz.media>
2023-04-21 17:45:49 +0200

    addons: powersched: fix typo
    Signed-off-by: AuxXxilium <info@visionz.media>

--- powersched/install.sh
+++ powersched/install.sh
@@ -2,14 +2,15 @@
 
 if [ `mount | grep tmpRoot | wc -l` -gt 0 ] ; then
 
-SED_PATH='/tmpRoot/usr/bin/sed'
+  SED_PATH='/tmpRoot/usr/bin/sed'
 
-if [ "${1}" = "late" ]; then
-  echo "Installing powersched tools"
-  cp -vf /usr/sbin/powersched /tmpRoot/usr/sbin/powersched
-  chmod 755 /tmpRoot/usr/sbin/powersched
-  # Clean old entries
-  ${SED_PATH} -i '/\/usr\/sbin\/powersched/d' /tmpRoot/etc/crontab 
-  # Add line to crontab, execute each minute
-  echo "*       *       *       *       *       root    /usr/sbin/powersched #arpl powersched addon" >> /tmpRoot/etc/crontab
+  if [ "${1}" = "late" ]; then
+    echo "Installing powersched tools"
+    cp -vf /usr/sbin/powersched /tmpRoot/usr/sbin/powersched
+    chmod 755 /tmpRoot/usr/sbin/powersched
+    # Clean old entries
+    ${SED_PATH} -i '/\/usr\/sbin\/powersched/d' /tmpRoot/etc/crontab 
+    # Add line to crontab, execute each minute
+    echo "*       *       *       *       *       root    /usr/sbin/powersched #arpl powersched addon" >> /tmpRoot/etc/crontab
+  fi
 fi
