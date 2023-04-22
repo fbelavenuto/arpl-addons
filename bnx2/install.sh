86fe35eb44c0fa2a5545474aa033efeb9c95bdef
AuxXxilium <info@visionz.media>
2023-04-22 14:24:34 +0200

    addons: remove bnx2 and 9p from addons
    Signed-off-by: AuxXxilium <info@visionz.media>

--- bnx2/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ash
-
-if [ "${1}" = "late" ]; then
-  echo "Installing Broadcom Netxtreme Gigabit Ethernet Driver"
-  tar -zxvf /addons/bnx2.tgz -C /tmpRoot/
-  chmod 755 /tmpRoot/usr/lib/firmware/bnx2/*.fw
-fi
